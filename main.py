9dd42b5a130a621f9a3c4328c3dbe4d2f061ae44
BradyDuPaul2023 <816947@mystma.org>
2021-03-21 22:46:04 +0000

    madlib

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 adjective1 = "very"
 adjective2 = "delicious"
-noun1 = "turkey"
+noun1 = "turkey."
 noun2 = "kitchen"
 verb1 = "ran"
-verb2 = "prepare"
-properNoun3 = "Sally"
+verb2 = "prepare."
 noun4 = "water"
 noun5 = "living"
-noun6 = "eyes"
+noun6 = "eyes,"
 noun7 = "dogs"
 verb3 = "running"
 noun8 = "table"
-print("It was a " + adjective1 + " ,cold November day. I woke up to the " + adjective2 + " smell of")
+print("It was a " + adjective1 + " ,cold November day. I woke up to the " + adjective2 + " smell of " + noun1 + " I hurried down to the " + noun2 + " and " + verb1 + " to help " + verb2 + " I got myself a glass of " + noun4 + " and moved to the " + noun5 + " room, when I got there I couldn't believe my " + noun6 + " there were so many " + noun7 + " and they were all " + verb3 + " on top of the " + noun8)
