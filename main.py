1116cc314bbfeb7d33e73415b8ca4a32787cdc6c
BradyDuPaul2023 <816947@mystma.org>
2021-03-22 15:26:41 +0000

    madlib

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-adjective1 = "very"
-adjective2 = "delicious"
-noun1 = "turkey."
-noun2 = "kitchen"
-verb1 = "ran"
-verb2 = "prepare."
-noun4 = "water"
-noun5 = "living"
-noun6 = "eyes,"
-noun7 = "dogs"
-verb3 = "running"
-noun8 = "table!"
+adjective1 = input("what is adjective 1?\n")
+adjective2 = input("what is adjective 2?\n")
+noun1 = input("what is noun1?\n")
+noun2 = input("what is noun2?\n")
+verb1 = input("what is verb1?\n")
+verb2 = input("what is verb2?\n")
+noun4 = input("what is noun4?\n")
+noun5 = input("what is noun5?\n")
+noun6 = input("what is noun6?\n")
+noun7 = input("what is noun7?\n")
+verb3 = input("what is verb3?\n")
+noun8 = input("what is noun8?\n")
 print("It was a " + adjective1 + " cold November day. I woke up to the " + adjective2 + " smell of " + noun1 + " I hurried down to the " + noun2 + " and " + verb1 + " to help " + verb2 + " I got myself a glass of " + noun4 + " and moved to the " + noun5 + " room, when I got there I couldn't believe my " + noun6 + " there were so many " + noun7 + " and they were all " + verb3 + " on top of the " + noun8)
