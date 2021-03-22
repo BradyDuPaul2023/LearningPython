6cb3dce475ade1b2b8df58e045c718867aa2a07a
BradyDuPaul2023 <816947@mystma.org>
2021-03-22 15:00:10 +0000

    madlib

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ noun6 = "eyes,"
 noun7 = "dogs"
 verb3 = "running"
 noun8 = "table!"
-print("It was a " + adjective1 + " ,cold November day. I woke up to the " + adjective2 + " smell of " + noun1 + " I hurried down to the " + noun2 + " and " + verb1 + " to help " + verb2 + " I got myself a glass of " + noun4 + " and moved to the " + noun5 + " room, when I got there I couldn't believe my " + noun6 + " there were so many " + noun7 + " and they were all " + verb3 + " on top of the " + noun8)
+print("It was a " + adjective1 + " cold November day. I woke up to the " + adjective2 + " smell of " + noun1 + " I hurried down to the " + noun2 + " and " + verb1 + " to help " + verb2 + " I got myself a glass of " + noun4 + " and moved to the " + noun5 + " room, when I got there I couldn't believe my " + noun6 + " there were so many " + noun7 + " and they were all " + verb3 + " on top of the " + noun8)
